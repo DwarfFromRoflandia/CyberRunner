e3882eb91e871410365fe75ad991528cd2e7a08c
Arsenij67 <96657896+Arsenij67@users.noreply.github.com>
2022-07-05 14:03:50 +0300

    Новый коммит

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public static class EventManager 
+{
+	public static Action<float> EventPlay;
+	// Start is called before the first frame update
+
+}
