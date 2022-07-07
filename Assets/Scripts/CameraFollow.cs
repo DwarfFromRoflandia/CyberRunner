5e6324a3f0cb8db103b3a25d147a76a31cf6e548
Arsenij67 <96657896+Arsenij67@users.noreply.github.com>
2022-07-07 13:02:56 +0300

    Добавил плавности при перемещении

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -14,11 +14,11 @@ public class CameraFollow : MonoBehaviour
     void LateUpdate()
     {
 
-        Vector3 EndPoint = new Vector3(transform.position.x, transform.position.y,  Player.transform.position.z-DistanceFromPlayer);
+        Vector3 EndPoint = new Vector3(Player.transform.position.x, transform.position.y,  Player.transform.position.z-DistanceFromPlayer);
 
         transform.position = Vector3.Lerp(transform.position, EndPoint,0002f*Time.deltaTime);
 
-        transform.LookAt(Player.transform);
+         
 
 
 
