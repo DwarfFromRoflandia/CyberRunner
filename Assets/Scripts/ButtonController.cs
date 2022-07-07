89951cc420412aa94b33a7e44e40aaa5714253da
Arsenij67 <96657896+Arsenij67@users.noreply.github.com>
2022-07-07 13:36:20 +0300

    Добавил кнопку Settings и немного отредачил сцену

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -7,17 +7,26 @@ public class ButtonController :MonoBehaviour
 {
    public float GameSpeed;
     public GameObject Main_Menu_Condition; // ссылка на главное меню
+    public GameObject Settings_Menu;
 
-
-
-    public void InputPlay() /* метод при использовании которого запускается игровая сессия 
+	private void Start()
+	{
+        Settings_Menu.SetActive(false);
+	}
+	public void InputPlay() /* метод при использовании которого запускается игровая сессия 
                               (При нажатии на кнопку Play в главном меню)*/
     {
         Main_Menu_Condition.SetActive(false);
         EventManager.EventPlay?.Invoke(GameSpeed);
     }
 
-     
+    public void InputSettings()
+    {
+        Main_Menu_Condition.SetActive(false);
+        Settings_Menu.SetActive(true);
+    
+    
+    }
 
     
 }
