949a49be2c5a14b4e0baef4d44d1100662ac0f9e
Arsenij67 <96657896+Arsenij67@users.noreply.github.com>
2022-07-16 15:52:29 +0300

    Импортировал недостающий интерфейс и написал код для звука
    ИНТЕРФЕЙС импортировал на будущее.Также реализовал кнопку включения и выключения звука

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-
+using UnityEngine.UI;
 public class ButtonController :OpenAndExitStore
 {
    public float GameSpeed;
     public GameObject Main_Menu_Condition; // ссылка на главное меню
     public GameObject Settings_Menu;
 
+    private AudioSource PhoneSource;
+    private Image MusicImage;
+
+    public Sprite MusicOn, MusicOff;
 	private void Start()
 	{
+        PhoneSource = GameObject.Find("MainMenuCanvas").GetComponent<AudioSource>();
+        MusicImage = GameObject.Find("ButtonMusic").GetComponent<Image>();
         Settings_Menu.SetActive(false);
+        PlayerPrefs.SetString("MusicCondition","On");
+        Music();
 	}
 	public void InputPlay() /* метод при использовании которого запускается игровая сессия 
                               (При нажатии на кнопку Play в главном меню)*/
@@ -35,6 +43,29 @@ public class ButtonController :OpenAndExitStore
     
     }
 
+    public void Music()
+    {
+        if (PlayerPrefs.GetString("MusicCondition") == "On")
+        {
+
+            PhoneSource.Play();
+            PlayerPrefs.SetString("MusicCondition", "Off");
+            MusicImage.sprite = MusicOn;
+
+
+        }
+
+        else if(PlayerPrefs.GetString("MusicCondition") =="Off")
+         {
+
+            PhoneSource.Stop();
+            PlayerPrefs.SetString("MusicCondition", "On");
+            MusicImage.sprite = MusicOff;
+        }
+    
+    
+    
+    }
 
 
 }
